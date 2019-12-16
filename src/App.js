6f6a6f4a7630cfda284661ff824ff09877a87036
unknown <fegoworks@hotmail.com>
2019-12-16 13:51:40 +0100

    feat(searchbar): add searchbar component

--- src/App.js
+++ src/App.js
@@ -2,15 +2,39 @@ import React from 'react';
 import { Grid } from '@material-ui/core';
 import youtube from './api/youtube';
 import { SearchBar, VideoList, VideoDetail } from './components';
+const API_KEY = process.env.REACT_APP_API_KEY;
 
 class App extends React.Component {
+  state = {
+    videos: [],
+    selectedVideo: null,
+  };
+
+  handleSubmit = async searchTerm => {
+    const response = await youtube.get('search', {
+      params: {
+        part: 'snippet',
+        maxResults: 5,
+        key: API_KEY,
+        q: searchTerm,
+      },
+    });
+
+    this.setState({
+      videos: response.data.items,
+      selectedVideo: response.data.items[0],
+    });
+
+    console.log(response.data.items);
+  };
+
   render() {
     return (
-      <Grid justify="center" container spacing={16}>
+      <Grid justify="center" container spacing={10}>
         <Grid item xs={12}>
-          <Grid container spacing={16}>
+          <Grid container spacing={10}>
             <Grid item xs={12}>
-              {/* SEARCH BAR*/}
+              <SearchBar onFormSubmit={this.handleSubmit} />
             </Grid>
             <Grid item xs={12}>
               {/* VIDEO DETAILS */}
